d6e893fc9bf670b922f9f9f65bc02f47845fdebb
Khezen <simonneaug@gmail.com>
2013-03-14 13:21:56 +0000

    fix du customSlider
    git-svn-id: https://dev.isen.fr/svn/2012-2013/cir3/java/project/group5/geneticAlgo/GeneticAlgorithm@28 84736ae8-fca8-4d2c-b905-760bd7953024

--- src/main/java/GraphicalComponents/CustomSlider.java
+++ src/main/java/GraphicalComponents/CustomSlider.java
@@ -6,31 +6,34 @@ package GraphicalComponents;
 
 import java.util.LinkedList;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  *
  * @author simonneau
  */
-public class CustomSlider extends IdentifiableComponent implements Observable, Observer {
+public class CustomSlider extends IdentifiableComponent implements Observable, Observer, ChangeListener {
 
     private LinkedList<Observer> observers = new LinkedList<>();
-    
     private JSlider slider;
 
     public CustomSlider(int min, int max, int value) {
         slider = new JSlider(min, max, value);
         this.add(slider);
+        this.slider.addChangeListener(this);
     }
-    
+
     public void setValue(int value) {
         this.setValue(value, true);
     }
 
     private void setValue(int value, boolean haveToNotify) {
 
-        this.slider.setValue(value);
-        if(haveToNotify){
+        if (haveToNotify) {
             this.notifyObserver();
+        } else {
+            this.slider.setValue(value);
         }
 
     }
@@ -42,30 +45,35 @@ public class CustomSlider extends IdentifiableComponent implements Observable, O
 
     @Override
     public void notifyObserver() {
-        for(Observer o : this.observers){
+        for (Observer o : this.observers) {
             o.reactToChanges(new CustomSliderEvent(this, this.slider.getValue()));
         }
     }
 
     @Override
     public void reactToChanges(ObservationEvent ev) {
-        CustomSliderEvent event = (CustomSliderEvent)ev;
+        CustomSliderEvent event = (CustomSliderEvent) ev;
         this.setValue(event.getValue(), false);
     }
-    
-    public void setMinimum(int minimum){
+
+    public void setMinimum(int minimum) {
         this.slider.setMinimum(minimum);
     }
-    
-    public void setPaintTicks(boolean bool){
+
+    public void setPaintTicks(boolean bool) {
         this.slider.setPaintTicks(bool);
     }
-    
-    public void setPaintLabels(boolean bool){
+
+    public void setPaintLabels(boolean bool) {
         this.slider.setPaintLabels(bool);
     }
-    
-    public void setMaximum(int maximum){
+
+    public void setMaximum(int maximum) {
         this.slider.setMaximum(maximum);
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent ce) {
+        this.setValue(this.slider.getValue());
     }
 }
