7e4024c8903d3bbb636cd5857cd5bf7342fb3815
Khezen <simonneaug@gmail.com>
2013-04-11 09:34:51 +0000

    GeneticEngineUI finished
    git-svn-id: https://dev.isen.fr/svn/2012-2013/cir3/java/project/group5/geneticAlgo/GeneticAlgorithm@60 84736ae8-fca8-4d2c-b905-760bd7953024

---
+++ src/main/java/GraphicalComponents/PauseStepPanel.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GraphicalComponents;
+
+import java.awt.Frame;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author simonneau
+ */
+public class PauseStepPanel extends IdentifiableComponent implements Observer, Observable {
+
+    private PauseButton pauseButton;
+    private ValidateButton stepButton;
+    private LinkedList<Observer> observers;
+
+    public PauseStepPanel() {
+        this(false);
+    }
+
+    public PauseStepPanel(boolean isPaused) {
+
+        this.observers = new LinkedList<>();
+        
+        this.pauseButton = new PauseButton(isPaused);
+        this.stepButton = new ValidateButton("step");
+        this.stepButton.setVisible(isPaused);
+
+        this.add(pauseButton);
+        this.add(stepButton);
+
+        this.pauseButton.addObserver(this);
+        this.stepButton.addObserver(this);
+
+    }
+
+    @Override
+    public void reactToChanges(ObservationEvent ev) {
+        IdentifiableComponent source = (IdentifiableComponent) (ev.getSource());
+        int id = source.getId();
+
+        if (id == this.pauseButton.getId()) {
+
+            PauseEvent pev = (PauseEvent) ev;
+            boolean paused = pev.isPaused();
+            
+            this.stepButton.setVisible(paused);
+            this.notifyObserver(new PauseEvent(this, paused));
+
+        } else if (id == this.stepButton.getId()) {
+
+            this.notifyObserver(new ValidateButtonEvent(this));
+        }
+    }
+
+    @Override
+    public void addObserver(Observer o) {
+        this.observers.add(o);
+    }
+
+    @Override
+    public void notifyObserver() {
+        //empty ==> have to be more specific
+    }
+
+    private void notifyObserver(ValidateButtonEvent ev) {
+        for (Observer o : observers) {
+            o.reactToChanges(ev);
+        }
+    }
+
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.add(new PauseStepPanel(false));
+        f.setVisible(true);
+    }
+    
+    
+}
